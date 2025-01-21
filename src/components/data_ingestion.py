197cbb9c929d6023db1ad4f67c6c67305a15c689
A-X-Z-Y-T-E <b23296@students.iitmandi.ac.in>
2025-01-21 10:56:04 +0530

    Added Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -42,8 +42,3 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(f"Data ingestion failed with error: {str(e)}", sys.exc_info())
         
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
